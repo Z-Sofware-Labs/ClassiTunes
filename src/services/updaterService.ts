9492399b84ce8d3f9df83507b7fb68e5b9f4917a
Pakwan Manuel Marquez <barreramarquez2003@gmail.com>
2026-09-14 15:56:50 +0800

    chore: bump version to 1.1.3

--- src/services/updaterService.ts
+++ src/services/updaterService.ts
@@ -28,7 +28,7 @@ export interface UpdateState {
   message?: string;
 }
 
-export const CURRENT_VERSION = '1.1.2';
+export const CURRENT_VERSION = '1.1.3';
 
 // Canonical repository information
 export const GITHUB_REPO = 'Z-Sofware-Labs/ClassiTunes';
